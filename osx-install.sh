f33e383d11a181581b8e1a4fcf3ae6b0a5890808
early evening <eve@haut-kultur.org>
2019-10-16 22:03:51 -0700

    use rsync instead of symlinks to install
    - max 7 has crashed when using symlinks for this

--- osx-install.sh
+++ osx-install.sh
@@ -2,4 +2,8 @@
 
 this_dir="$(dirname $0)"
 
-ln -s $this_dir ~/Documents/Max\ 7/Library/eve-generics
+dest="${HOME}/Documents/Max 7/Library/eve-generics"
+
+mkdir -p "${dest}"
+
+rsync -avh ${this_dir}/ "${dest}"/
